9d94c7e94cfa14264186c621623a69f7fd8ad1f6
席萍萍Brook.dinglan <brook.dinglan@lazada.com>
2022-01-06 08:50:39 +0800

    add JSONString property wrapper

---
+++ sources/JSONString.swift
@@ -0,0 +1,33 @@
+//
+//  JSONString.swift
+//  OOCodable
+//
+//  Created by beforeold on 2021/11/29.
+//  Copyright © 2021 OOCodable. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct JSONString<Base: Codable>: Codable {
+    public var wrappedValue: Base
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let string = try? container.decode(String.self),
+           let data = string.data(using: .utf8) {
+            self.wrappedValue = try LionCoderFactory.newJSONDecoder().decode(Base.self, from: data)
+            return
+        }
+        
+        self.wrappedValue = try container.decode(Base.self)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let data = try LionCoderFactory.newJSONEncoder().encode(wrappedValue)
+        if let string = String(data: data, encoding: .utf8) {
+            try container.encode(string)
+        }
+    }
+}
