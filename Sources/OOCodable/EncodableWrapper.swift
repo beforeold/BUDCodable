05f98cc36ac921f150154cf851e3619a705d04d7
beforeold <ibeforeold@gmail.com>
2022-09-19 21:06:31 +0800

    let EncodableWrapper support propertyWrapper

--- Sources/OOCodable/EncodableWrapper.swift
+++ Sources/OOCodable/EncodableWrapper.swift
@@ -10,16 +10,32 @@ import Foundation
 /// erase Encodable value in collection types
 ///
 /// transform example: [1, "2", 3.0].map(EncodableWrapper.init)
+///
+@propertyWrapper
 public struct EncodableWrapper {
-    public let wrapped: Encodable
+    private var value: Encodable
     
-    public init(_ wrapped: Encodable) {
-        self.wrapped = wrapped
+    public init(wrappedValue: Encodable) {
+        self.value = wrappedValue
+    }
+    
+    public var wrappedValue: Encodable {
+        set {
+            value = newValue
+        }
+        
+        get {
+            return value
+        }
+    }
+    
+    public var projectedValue: EncodableWrapper {
+        return self
     }
 }
 
 extension EncodableWrapper: Encodable {
     public func encode(to encoder: Encoder) throws {
-        try wrapped.encode(to: encoder)
+        try wrappedValue.encode(to: encoder)
     }
 }
